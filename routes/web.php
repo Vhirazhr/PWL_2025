ff67e02b2c0b4d26091111f5b8f629676b4e8882
Vhirazhr <fakhirazahra88@gmail.com>
2025-02-20 12:09:45 +0700

    JS2-Route NIM

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/world', function () {
-    return 'world';
+Route::get('/welcome', function () {
+    return 'Fakhira zahrany 2341760172';
 });
