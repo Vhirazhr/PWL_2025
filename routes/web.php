9df26d622d6892202e494b66aacae54bffa9d76e
Vhirazhr <fakhirazahra88@gmail.com>
2025-02-20 13:29:57 +0700

    JS2-Route Articles

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
-    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+Route::get('/Articles/{id}', function ($id) {
+    return " Article Page with ID : ".$id;
     });
     
